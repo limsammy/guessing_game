2c7df732ebb58599ba631ccfc16d1a32756f8674
Sam Lim <limsammy1@gmail.com>
2017-03-09 23:37:47 -0800

    Added hints, finished game

--- guessing_game.rb
+++ guessing_game.rb
@@ -17,21 +17,35 @@ end
 @n = Number.new
 
 def game
+
+	if(@n.value.even?)
+		hint = "even"
+	else
+		hint = "odd"
+	end
+
 	loop do
+
 		print("Please input your guess or enter the secret cheat command > ")
 		input = gets.chomp
-		# binding.pry
+		
 		if(input.to_s == "c" || input.to_s == "cheat")
 			puts "Expected answer: #{@n.value}"
 		elsif(input.to_i > @n.value)
-			puts "Your guess is higher than the actual value! Please guess again..."
+			puts "Your guess is MORE THAN than the actual value!"
+			puts "Hint: Your number is #{hint}."
+			puts "Please guess again..."
 		elsif(input.to_i < @n.value)
-			puts "Your guess is lower than the actual value! Please guess again..."
+			puts "Your guess is LESS THAN than the actual value!"
+			puts "Hint: Your number is #{hint}."
+			puts "Please guess again..."
 		elsif(input.to_i == @n.value)
 			puts "You guessed #{input} correctly!"
 			break
 		end
+
 	end
+
 end
 
 game()
