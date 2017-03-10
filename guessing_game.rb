91430715f29b7cbd12f43f37517cb7ab0818012b
Sam Lim <limsammy1@gmail.com>
2017-03-09 23:17:42 -0800

    Added game method: asks for user guess and checks it against @n.value (the real value)

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 puts "I have generated a random number for you to guess, what is your guess?"
 
 # generate random number between 1 and 100
@@ -8,5 +10,23 @@ class Number
 	def initialize
 		@value = rand(1..100)
 	end
-	
+
 end
+
+# prompt user to guess number
+@n = Number.new
+
+def game
+	loop do
+		print("Please input your guess > ")
+		input = gets.chomp.to_i
+		if(input != @n.value)
+			puts "Guess again..."
+		else
+			puts "You guessed #{input} correctly!"
+			break
+		end
+	end
+end
+
+game()
